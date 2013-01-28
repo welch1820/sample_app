4deb54da4d72737cd5247762037a32f3e46da6ef
Bill Welch <bill_welch@kace.com>
2013-01-28 16:50:28 -0500

    10.2.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
-#  validates_confirmation_of :password
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
